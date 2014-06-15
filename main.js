e5c33fde82e8e89a57ecf68394b6ee475b1a00c8
Tomas Dvorak <todvora@gmail.com>
2014-06-15 15:35:08 +0200

    better logic, handles comments, recursion based

--- main.js
+++ main.js
@@ -1,32 +1,59 @@
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str){
+        return this.indexOf(str) == 0;
+    };
+}
+
 var joinParts = function(parts) {
     return eval("'"+parts.join("=")+"'");
-}
+};
+
+var createCommentElement = function(line) {
+    return "<!-- " + line + " -->";
+};
+
+var createEntryElement = function(key, value) {
+    return '<entry key="' + key + '">' + value + '</entry>';
+};
+
+var formatLineOutput = function(output, separator) {
+    return separator + output + "\n";
+} ;
+
+var parseLines = function(lines, separator) {
+    if(lines.length == 0) {
+        return "";
+    }
+
+    var line = lines.shift().trim();
+    console.log("Line:" + line);
+
+    if(line.length == 0) {
+        return formatLineOutput("", separator) + parseLines(lines, separator);
+    }
+
+    if(line.startsWith("!") || line.startsWith("#")) {
+        return formatLineOutput(createCommentElement(line.substr(1)), separator) + parseLines(lines, separator);
+    }
+
+    var parts = line.split("=");
+    var key = parts.shift();
+    var value = joinParts(parts);
+    return  formatLineOutput(createEntryElement(key,value), separator) + parseLines(lines, separator);
+};
+
 
 var transform = function(text, separator) {
     var result = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">\n<properties>';
     var lines = text.split('\n');
-    for(var i = 0;i < lines.length;i++){
-        var line = lines[i];
-        var parts = line.split('=');
-        if(parts.length > 1) {
-            result = result + '\n'+separator+'<entry key="'+parts.shift().trim()+'">'+joinParts(parts)+'</entry>'
-        } else {
-            if(line.trim().length == 0) {
-                result = result + "\n";
-            }
-           // for now, ignore, later lint and print error
-        }
-    }
-    var result = result + "\n</properties>"
+    result = result + "\n" + parseLines(lines, separator) + "</properties>";
     return result;
-}
+};
 
 $(document).ready(function() {
     $("#convert").click(function() {
-        var source = $("#source").val();
-        console.log(source);
+        var source = $("#source").val().trim();
         var transformed = transform(source, "    ");
-        console.log(transformed);
         $("#target").val(transformed);
     });
 });
